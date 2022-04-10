8d7bcace1c26e79ae0b96ec1396be3eea99b51d4
Jin-Mook <jink0608@naver.com>
2022-04-10 17:41:30 +0900

    git push 테스트 코드 원상태로 복구

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,10 +19,6 @@ app.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
   }
 });
 
-app.get('/', (req, res, next) => {
-  res.send('hello');
-});
-
 app.get('/redis/:word', async (req, res, next) => {
   try {
     const newWord = req.params.word;
